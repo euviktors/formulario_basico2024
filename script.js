196ebfd7eb91e1fe6c9a9bcfe0f56f8c7d27d997
Viktor <110547300+euviktors@users.noreply.github.com>
2024-01-15 12:26:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function enviarFormulario() {
+    // Aqui você pode adicionar a lógica para enviar os dados do formulário
+    // Exemplo: validar campos, enviar para um servidor, etc.
+    alert('Formulário enviado com sucesso!');
+}
